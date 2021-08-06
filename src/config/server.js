476708148d1b33841251a2c65ee5411b419e332e
Walther Chavarro <chavarrowalther01@gmail.com>
2021-08-06 09:28:10 -0500

    Cambio en el serve.js

--- src/config/server.js
+++ src/config/server.js
@@ -7,14 +7,21 @@ const session = require ('express-session');
 const app = express ();
 
 app.set('port', process.env.PORT || 3000);
+
+
+// Gestor  de plantillas html-ejs
 app.set('views engine', 'ejs');
+
+//Rutas de las vistas
 app.set('views', path.join(__dirname, '../app/views'));
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
+//env
 dotenv.config({path: path.join(__dirname, '../env/.env')});
 
+// public
 app.use('/resources', express.static(path.join(__dirname, '../public')));
 
 app.use(session({
